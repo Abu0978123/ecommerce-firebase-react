d04a62f5240ef212114d9bb4d50984d47a010e9f
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-27 14:30:00 +0500

    filter added in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import Layout from '../../components/Layout/Layout'
 import MyContext from '../../context/data/myContext'
 import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
 
 const Home = () => {
   const context = useContext(MyContext)
   return (
     <Layout>
     <HeroSection/>
+    <Filter/>
     </Layout>
   )
 }
