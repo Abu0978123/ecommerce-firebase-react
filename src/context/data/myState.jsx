951d837f1f78f51b0554a85d87b46189154e6255
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2023-12-27 12:54:04 +0500

    navbar styled with light dark mode

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MyContext from "./myContext";
 const MyState = (props) => {
-  const state = {
-    name: "abu bakar",
-    profession: "software engineer",
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17, 24, 39)";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
   };
-  return <MyContext.Provider value={state}>{props.children}</MyContext.Provider>;
+  return (
+    <MyContext.Provider value={{ mode, toggleMode }}>
+      {props.children}
+    </MyContext.Provider>
+  );
 };
 
 export default MyState;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,13 +4,9 @@ import MyContext from '../../context/data/myContext'
 
 const Home = () => {
   const context = useContext(MyContext)
-  const {name, profession} = context;
-  console.log(name ,profession)
   return (
     <Layout>
-      my name is {name}
-      and 
-      my profession is {profession}
+    Home page
     </Layout>
   )
 }
